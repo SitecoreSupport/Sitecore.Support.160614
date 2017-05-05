4d1029e65531c5feb6b24d6a71bc6eeff151b9f3
Pushpraj Ruhal <pur@sitecore.net>
2017-05-05 10:46:32 +0800

    Fixes issues with WFFM send email action when fields are inserted at beginning of e-mail in IE

---
+++ src/Sitecore.Support.160614/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.160614")]
+[assembly: AssemblyProduct("Sitecore.Support.160614")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Fixes issues with WFFM send email action when fields are inserted at beginning of e-mail in IE")]
+
