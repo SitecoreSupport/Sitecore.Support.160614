358f1865d5437ee3c278caff0082e7a105f624a7
AriaAkbariyeh <ara@sitecore.net>
2017-10-25 14:18:49 +0800

    Import original WFFM 8.1.3 code

---
+++ src/Sitecore.Support.160614/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.160614")]
+[assembly: AssemblyProduct("Sitecore.Support.160614")]
+[assembly: ComVisible(false)]
